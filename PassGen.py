49cafc818f4d45731e50d6abf3b4cbe49d9fae51
Merhaba2232 <108890613+Merhaba2232@users.noreply.github.com>
2023-05-16 21:00:57 +0300

    Add files via upload

--- PassGen.py
+++ PassGen.py
@@ -1,6 +1,5 @@
 import random
 
-print("Generating Password")
 PasswordLenght = input("Password Lenght: ")
 
 charlist = "abcdefghıjklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!'^+%&/()=?>£#${[]}}|"
@@ -10,9 +9,8 @@ genpass = []
 while 3 == 3:
 	GeneratedChars = GeneratedChars + 1
 	if GeneratedChars == int(PasswordLenght) + 1:
-		break
+		break	
 
-	
 	GenerateChar = random.randint(0,len(charlist) -1)
 	genpass.append(charlist[GenerateChar])
 
